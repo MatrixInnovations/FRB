b021a86e5829ad85eecd11f1105248f25dc4f6b5
Geekazon <130860322+MatrixInnovations@users.noreply.github.com>
2024-01-11 18:50:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-import GreenBackgroundVideo from './assets/GreenBackground.mp4'; // Update the path accordingly
+import GreenBackgroundVideo from './assets/GreenBackground.mp4'; 
 
 const initialState = {
       input: '',

0176e5f119a577dee0d4b92788bc0a64b06c4bbc
Geekazon <130860322+MatrixInnovations@users.noreply.github.com>
2024-01-13 19:32:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('postgres://mindgamesdb_qvf3_user:brdXDP9eAIVIUcmWROGuOcSv7CaDzx8w@dpg-cmfe53mn7f5s73c4ej70-a/mindgamesdb_qvf3', {
+          fetch('https://mindgamesbe.onrender.com', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
